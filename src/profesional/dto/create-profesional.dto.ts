e336d3e09390971ab2cc98dd43f5092a83651b8a
tomas <tmshd13@gmail.com>
2023-11-22 19:00:09 -0300

    respuesta de error al registro

--- src/profesional/dto/create-profesional.dto.ts
+++ src/profesional/dto/create-profesional.dto.ts
@@ -1,21 +1,37 @@
-import { IsNumber, IsString } from "class-validator"
+import { IsNotEmpty, IsNumber, IsString, Validate, ValidationArguments  } from "class-validator"
+
+// function IsNotOnlyWhitespace(value: string, validationArguments: ValidationArguments) {
+//     if (!value || value.trim().length === 0) {
+//         return false;
+//     }
+//     return true;
+// }
 
 export class CreateProfesionalDto {
+    @IsNotEmpty()
     @IsString()
     nombre: string;
 
+    @IsNotEmpty()
     @IsString()
     apellido: string;
 
+    @IsNotEmpty()
     @IsString()
     correo: string;
 
+    @IsNotEmpty()
     @IsString()
     contrasena: string;
 
+    @IsNotEmpty()
     @IsNumber()
     nroTelefono: string;
 
-    @IsString()
+    @IsNotEmpty()
+    // @IsString()
+    // @Validate(IsNotOnlyWhitespace, {
+    //     message: "El campo '{{ property }}' no puede ser un espacio en blanco"
+    // })
     tipoCuenta: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,21 +1,27 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateUserDto {
+    @IsNotEmpty()
     @IsString()
     nombre: string;
 
+    @IsNotEmpty()
     @IsString()
     apellido: string;
 
+    @IsNotEmpty()
     @IsString()
     correo: string;
 
+    @IsNotEmpty()
     @IsString()
     contrasena: string;
 
+    @IsNotEmpty()
     @IsNumber()
     nroTelefono: string;
 
+    @IsNotEmpty()
     @IsString()
     tipoCuenta: string;
 }
